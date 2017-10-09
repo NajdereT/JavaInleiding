a681485595e25fe4880fc2b4948a4159a5f23cc5
Najdere <NajdereJ@live.nl>
2017-10-09 11:37:11 +0200

    Java-inleiding Najdère

--- src/Java_Inleiding/Hoofdstuk11/Opdracht1.java
+++ src/Java_Inleiding/Hoofdstuk11/Opdracht1.java
@@ -2,16 +2,27 @@ package Java_Inleiding.Hoofdstuk11;
 
 import java.awt.*;
 import java.applet.*;
+import java.awt.event.*;
 
-public class Opdracht1 {
 
-    public void init() {
+public class Opdracht1 extends Applet {
+
+    public void init() {this.setSize(new Dimension(600,300));
     }
 
     public void paint(Graphics g) {
-        g.drawLine(50,5,5,300);
+        g.drawLine(800,50,80,300);
+        g.drawLine(100,50,100,300);
+        g.drawLine(120,50,120,300);
+        g.drawLine(140,50,140,300);
+        g.drawLine(160,50,160,300);
+        g.drawLine(180,50,180,300);
+        g.drawLine(200,50,200,300);
+        g.drawLine(220,50,220,300);
+        g.drawLine(240,50,240,300);
+        g.drawLine(260,50,260,300);
     }
 
-    ;
+
 
 }
---
+++ src/Java_Inleiding/Hoofdstuk11/Opdracht2.java
@@ -0,0 +1,14 @@
+package Java_Inleiding.Hoofdstuk11;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht2 extends Applet {
+    public void init (){
+        this.setSize(600,300);}
+        public void paint(Graphics g) {
+        int teller;
+        int y = 0;
+        g.drawString(" 20 19 18 17 16 15 14 13 12 11 10",20,20);
+        }
+}
