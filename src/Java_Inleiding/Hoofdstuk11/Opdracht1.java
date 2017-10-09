a65b2ea801de4f3b34a6c10b2d7363ab4301f436
Najdere <NajdereJ@live.nl>
2017-10-09 09:29:47 +0200

    Java-inleiding Najdère

---
+++ src/Java_Inleiding/Hoofdstuk11/Opdracht1.java
@@ -0,0 +1,17 @@
+package Java_Inleiding.Hoofdstuk11;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50,5,5,300);
+    }
+
+    ;
+
+}
